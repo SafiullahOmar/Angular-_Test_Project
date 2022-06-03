c923fd7bd49a52d7a89228d88bcfd5e30d4dc601
Safiullah Omar <omar.safiullah@gmail.com>
2022-06-03 09:32:14 +0430

    Children Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { AboutComponent } from './about/about.component';
+import { EmployeeAddComponent } from './employee-add/employee-add.component';
+import { EmployeeListingComponent } from './employee-listing/employee-listing.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"about",component:AboutComponent ,children:[{path:"company",component:AboutCompanyComponent}]},
+  {path:"employee",component:EmployeeComponent, children:[
+    {path:"",component:EmployeeListingComponent},
+    {path:"Edit/:id",component:EmployeeAddComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { EmployeeListingComponent } from './employee-listing/employee-listing.component';
+import { EmployeeAddComponent } from './employee-add/employee-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    HomeComponent,
+    AboutComponent,
+    AboutCompanyComponent,
+    EmployeeListingComponent,
+    EmployeeAddComponent
   ],
   imports: [
     BrowserModule,
