fb1809bece33854f392f9cfe72755568c3b874d7
Safiullah Omar <omar.safiullah@gmail.com>
2022-06-04 06:59:26 +0430

    Compare validators and form validation

--- src/app/formvalidation/formvalidation.component.ts
+++ src/app/formvalidation/formvalidation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-formvalidation',
@@ -7,7 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormvalidationComponent implements OnInit {
 
-  constructor() { }
+  reactiveform!: FormGroup;
+
+  constructor(private formbuilder: FormBuilder) {
+    this.reactiveform = formbuilder.group({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', Validators.required)
+    },
+    {
+      validators:this.match('password','confirmPassword')
+    }
+    );
+
+  }
+
+  get f() {
+    return this.reactiveform.controls;
+  }
+
+  match(password: any, confirmPassword: any) {
+    return (formgroup: FormGroup) => {
+      const passwordControl = formgroup.controls[password];
+      const confirmControl = formgroup.controls[confirmPassword];
+      if (confirmControl.errors && !confirmControl.errors['match'])
+        return;
+
+        if(passwordControl.value!==confirmControl.value){
+          confirmControl.setErrors({match:true});
+        }
+        else
+        confirmControl.setErrors(null);
+
+
+    }
+  }
+
 
   ngOnInit(): void {
   }
